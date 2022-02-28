39cd9f024d3777419e514afe70aac7cb01e6b39a
pollitodetroya <ignaciogovo@gmail.com>
2022-02-28 23:04:36 +0100

    Mejora en la busqueda del equipo en sofifa y en la busqueda de datos

--- webScraping/pruebas/serieA.py
+++ webScraping/pruebas/serieA.py
@@ -3,10 +3,10 @@ import requests
 import time
 import sofifa
 # link pagina:
-url = "https://us.as.com/resultados/futbol/italia/clasificacion/"
+url = "https://onefootball.com/es/competicion/serie-a-13/clasificacion"
 page = requests.get(url) # Optenemos la pagina
 soup = BeautifulSoup(page.content,'html.parser')
-nombre_equipo = soup.find_all("span", class_="nombre-equipo")
+nombre_equipo = soup.find_all("p", class_="title-7-medium standings__team-name")
 equipos = list()
 count = 0
 for i in nombre_equipo:
@@ -14,8 +14,8 @@ for i in nombre_equipo:
         break
     count +=1
     equipo = i.text
-    if equipo == "Nápoles":
-        equipo = "Napoli"
+    # if equipo == "Nápoles":
+    #     equipo = "Napoli"
     equipos.append(equipo) # Cogemos solo el texto de la etiqueta span
 for i in range(0,10):
     print("puesto: %d equipo: %s" % (i+1, equipos[i]))
